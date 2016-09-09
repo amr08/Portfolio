46bbabf640beec7e72a8675e85e985d9245db2fe
Andrea R <amrdev08@gmail.com>
2016-09-09 15:41:58 -0400

    nope this

--- index.php
+++ index.php
@@ -1,4 +1,1 @@
 <?php include_once("index.html", "giphyapp_1/index.html", "traintime_1/index.html","triviagame_1/index.html"); ?>
-<?php include_once("giphyapp_1/index.html"); ?>
-<?php include_once("traintime_1/index.html"); ?>
-<?php include_once("triviagame_1/index.html"); ?>
