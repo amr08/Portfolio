a3f6119f2069a1bb716c65110b4851d6315d78e7
Andrea R <amrdev08@gmail.com>
2016-09-09 15:50:14 -0400

    ...

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php include_once("index.html");
+include_once("ajax.html");
 include_once("giphyapp_1/index.html");
 include_once("triviagame_1/index.html");
 include_once("traintime_1/index.html"); ?>
